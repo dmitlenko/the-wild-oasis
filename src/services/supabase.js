4a7037d2549d50e0eafbf684ae942baa6e26b02d
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-02 13:37:27 +0300

    feat: Add @supabase/supabase-js package and configure Supabase client

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ecsruqdngyperqgymqgs.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjc3J1cWRuZ3lwZXJxZ3ltcWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4NjMxNzEsImV4cCI6MjA0MzQzOTE3MX0.oFam4rcwer1ImJNotO8p9Y95rEgi2lacuCTMJLmsF3Y';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
