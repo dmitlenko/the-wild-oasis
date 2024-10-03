4009db8e262890209c90c26cc2d32446fa9049f0
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-03 12:27:21 +0300

    refactor(apiCabins): prevent image uploading if it already exists

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -45,6 +45,8 @@ export async function createEditCabin(newCabin, id) {
     throw new Error('Cabin could not be created');
   }
 
+  if (hasImagePath) return data;
+
   // 2. Upload cabin image
   const { error: storageError } = await supabase.storage
     .from('cabin-images')
