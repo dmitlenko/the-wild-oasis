6a3c4ea086b594ae2a55ad1ca02ca9d35e73ab36
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-04 20:28:32 +0300

    refactor(authentication): update useLogin hook to fix redirection bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,21 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export function useLogin() {
   const navigate = useNavigate();
-  const { mutate, error, isPending } = useMutation({
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useMutation({
     mutationFn: login,
-    onSuccess: () => navigate('/dashboard', { replace: true }),
+    onSuccess: () => {
+      navigate('/dashboard', { replace: true });
+
+      // This fixes weird redirection bug
+      queryClient.invalidateQueries({
+        queryKey: ['user'],
+      });
+    },
     onError: () => toast.error('Provided email or password is incorrect'),
   });
 
