906dbbc6a03e076534278913ea1a390640eb0860
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-04 12:39:52 +0300

    refactor(ui): update AddCabin component to use new Modal component structure

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react';
 import Button from '../../ui/Button';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
 
 export default function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  const handleClose = () => setIsOpenModal(false);
-
   return (
-    <>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-
-      {isOpenModal && (
-        <Modal onClose={handleClose}>
-          <CreateCabinForm onCloseModal={handleClose} />
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
