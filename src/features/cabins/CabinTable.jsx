f272da6a6f3b48bf59ecb29fd189e7825bdcae4f
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-04 15:08:17 +0300

    refactor(ui): update CabinTable component show message when no items loaded

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,12 +5,15 @@ import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
 import { useSearchParams } from 'react-router-dom';
 import { filter, sort } from '../../utils/filtering';
+import Empty from '../../ui/Empty';
 
 function CabinTable() {
   const { isPending, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isPending) return <Spinner />;
+
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   const filteredCabins = filter({
     selectedFilter: searchParams.get('discount'),
