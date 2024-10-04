8e911136d2c8ec5a64e2842925a4965d7f9cc4d0
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-04 19:09:17 +0300

    refactor(ui): update CheckinBooking and BookingRow components
    - update imports and useBooking hook in CheckinBooking component
    - update imports and useBooking hook in BookingRow component
    - add check out functionality in CheckinBooking and BookingRow components

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,8 +11,9 @@ import ButtonText from '../../ui/ButtonText';
 import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from './useBooking';
 import Spinner from '../../ui/Spinner';
-import { HiArrowDownOnSquare } from 'react-icons/hi2';
+import { HiArrowDownOnSquare, HiArrowUpOnSquare } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
+import { useCheckout } from '../check-in-out/useCheckout';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,6 +23,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const navigate = useNavigate();
+  const { checkOut, isCheckingOut } = useCheckout();
   const { booking, isPending } = useBooking();
   const { status, id: bookingId } = booking ?? {};
 
@@ -54,6 +56,15 @@ function BookingDetail() {
             onClick={() => navigate(`/bookings/${bookingId}/checkin`)}
           >
             Check in
+          </Button>
+        )}
+        {status === 'checked-in' && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkOut(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
           </Button>
         )}
         <Button variation="secondary" onClick={moveBack}>
