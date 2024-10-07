8c72e50c4dfbef03336e0e39fa180bc66c2cb725
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-07 13:10:33 +0300

    refactor(ui): update App component to include DarkModeProvider

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect } from 'react';
+import { useLocalStorageState } from '../hooks/useLocalStorageState';
+
+const DarkModeContext = createContext();
+
+export default function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  useEffect(() => {
+    const cl = document.documentElement.classList;
+
+    if (isDarkMode) {
+      cl.add('dark-mode');
+      cl.remove('light-mode');
+    } else {
+      cl.add('light-mode');
+      cl.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+
+  return (
+    <DarkModeContext.Provider
+      value={{
+        isDarkMode,
+        toggleDarkMode,
+      }}
+    >
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (!context)
+    throw new Error('useDarkMode must be used only inside DarkModeContext');
+
+  return context;
+}
