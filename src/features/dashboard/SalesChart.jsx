7360d4d100b6c4b2db9ad1c6af4fe4c11a10320b
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-07 14:05:39 +0300

    refactor(dashboard): update SalesChart heading to display date range

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -57,7 +57,10 @@ export default function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {format(allDates.at(0), 'MMM dd yyyy')} to{' '}
+        {format(allDates.at(-1), 'MMM dd yyyy')}
+      </Heading>
       <ResponsiveContainer width="100%" height={300}>
         <AreaChart data={data}>
           <XAxis
