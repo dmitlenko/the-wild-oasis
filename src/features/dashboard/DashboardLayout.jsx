b7abedbcd2d50ed955f283b7920476caacd7d95d
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-07 14:43:17 +0300

    refactor(dashboard): update DashboardLayout component
    - add TodayActivity component to display today's activity
    - add DurationChart component to show duration of stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,8 @@ import { useRecentBookings } from './useRecentBookings';
 import Spinner from '../../ui/Spinner';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,6 +35,8 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
